a875d8707c5982369380684c86e74c9c2305eb6e
Preetha Krishnan <preetha.krishnan@softwire.com>
2025-04-24 17:17:27 +0100

    added code to return nearest bus stop ids

--- busboard.js
+++ busboard.js
@@ -1,29 +1,41 @@
 import fetch from 'node-fetch'
 import { config } from 'dotenv';
 import { trace } from 'console';
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync();
 
 const api_key = process.env.API_KEY;
 
 config()
 
+let postcode = prompt("Please enter a postcode ");
 const busStopCode = '490008660N';
-let postcode = 'BR49EW';
 let stopType = 'NaptanPublicBusCoachTram';
 
 async function getArrivalsFromTfl(stopPointId) {
-    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopPointId}/Arrivals?api_key=${api_key}`);
-    const responseBody = await response.json();
-    return responseBody;
+    try {
+        const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopPointId}/Arrivals?api_key=${api_key}`);
+        const responseBody = await response.json();
+        return responseBody;
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
 async function getBusStopArrivals(busStopId) {
-    let busData = await getArrivalsFromTfl(busStopId);
-    busData.slice(0, 5).forEach(element => {
-        let timeToWait = Math.floor(element.timeToStation /60) 
-        console.log(element.lineName + ', ' + element.towards + ', ' + `${timeToWait} minutes`);
-    });
+    try {
+        let busData = await getArrivalsFromTfl(busStopId);
+        busData.slice(0, 5).forEach(element => {
+            let timeToWait = Math.floor(element.timeToStation / 60)
+            console.log(element.lineName + ', ' + element.towards + ', ' + `${timeToWait} minutes`);
+        });
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
-
 async function getPostCodeData(postcode) {
     try {
         const response = await fetch(`https://api.postcodes.io/postcodes/${postcode}/`);
@@ -36,25 +48,30 @@ async function getPostCodeData(postcode) {
 
 async function getStopPoints(coordinates) {
     try {
-        const response = await fetch (`https://api.tfl.gov.uk/StopPoint/?lat=${coordinates[0]}&lon=${coordinates[1]}&stopTypes=${stopType}`);
+        const response = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${coordinates[0]}&lon=${coordinates[1]}&stopTypes=${stopType}`);
         const responseBody = await response.json();
-        return responseBody;
-    
+        return responseBody.stopPoints;
     } catch (err) {
         console.log(err);
     }
 }
 
+
 // getBusStopArrivals(busStopCode);
 
 async function transportSpots(postcode) {
-    let coordinates = await getPostCodeData(postcode);
-    let result = await getStopPoints(coordinates);
-    return result.stopPoints[0].modes;
+    try {
+        let responseArray = [];
+        let coordinates = await getPostCodeData(postcode);
+        let result = await getStopPoints(coordinates);
+        result.forEach(e => {
+            responseArray.push(e.naptanId);
+        })
+        return responseArray;
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
 console.log(await transportSpots(postcode));
-
-
-
-
